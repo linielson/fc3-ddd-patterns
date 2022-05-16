e1949e6a97074cb3b59e0c81c32c8031e9bf1a27
Linielson Rosa <linielsonrosa@gmail.com>
2022-05-16 10:20:43 -0300

    Create Product Domain Event

---
+++ src/domain/event/product/product-created.event.ts
@@ -0,0 +1,12 @@
+import EventInterface from "../@shared/event/event.interface";
+
+export default class ProductCreatedEvent implements EventInterface {
+  dataTimeOccurred: Date;
+  eventData: any;
+
+  constructor(eventData: any) {
+    this.dataTimeOccurred = new Date();
+    this.eventData = eventData;
+    // Create a new Product with the event data
+  }
+}
